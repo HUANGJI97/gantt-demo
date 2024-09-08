80ef3b921a42519cfaf2916a0a6ec89ffe734b88
慌雞 <lianfangti@qq.com>
2024-09-09 00:12:04 +0800

    feat: 自定义表格新增展开收起功能

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
 import "./App.scss";
-import { Gantt, Task } from "gantt-task-react";
+import { Gantt, Task, ViewMode } from "gantt-task-react";
 import "gantt-task-react/dist/index.css";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { getTasks } from "./apis/tasks";
 import CustomTooltip from "./components/CustomTooltip";
 import GithubSvg from "./assets/github.svg";
 import useCustomTable from "./components/CustomTable";
 import dayjs from "dayjs";
+import "dayjs/locale/zh-cn"; // 引入中文语言包
+import ButtonTabs from "./components/ButtonTabs";
 
+dayjs.locale("zh-cn"); // 设置为中文
 function App() {
   // const [count, setCount] = useState(0)
   const [tasks, setTasks] = useState<Task[]>([]);
+  const [viewMode, setViewMode] = useState<ViewMode>(ViewMode.Day);
   const [TaskListHeader, TaskListTable] = useCustomTable([
-    { title: "名称", prop: "name" },
+    { title: "名称", prop: "name", onExpanderClick: handleExpanderClick },
     {
       title: "开始时间",
       prop: "start",
@@ -24,6 +28,18 @@ function App() {
       render: (val) => dayjs(val).format("YYYY-MM-DD"),
     },
   ]);
+
+  const columnWidth = useMemo(() => {
+    const DEFAULT = 60;
+    const modeColumnWidthMap = new Map<ViewMode, number>([
+      [ViewMode.Day, 60],
+      [ViewMode.Week, 250],
+      [ViewMode.Month, 300],
+      [ViewMode.QuarterYear, 400],
+      [ViewMode.Year, 450],
+    ]);
+    return modeColumnWidthMap.get(viewMode) || DEFAULT;
+  }, [viewMode]);
 
   async function fetchTask() {
     const result = await getTasks();
@@ -41,7 +57,41 @@ function App() {
     console.log("展开按钮", task);
     updateTask(task);
   }
+  function handleDateChange(task: Task) {
+    updateTask(task);
+  }
+  function handleProgressChange(task: Task) {
+    updateTask(task);
+  }
+  const customCalendarColumn = (date: Date) => {
+    const day = dayjs(date);
+    return (
+      <div className="gantt-calendar-column">
+        <div className="date">{day.format("MM-DD")}</div>
+        <div className="week">{day.format("ddd")}</div>
+      </div>
+    );
+  };
   // const CustomTooltip =
+  const viewModes = [
+    {
+      value: ViewMode.Day,
+      label: "日",
+    },
+
+    {
+      value: ViewMode.Week,
+      label: "周",
+    },
+    {
+      value: ViewMode.Month,
+      label: "月",
+    },
+    {
+      value: ViewMode.QuarterYear,
+      label: "季",
+    },
+  ];
 
   return (
     <div className="container">
@@ -56,16 +106,30 @@ function App() {
           <h1>Gantt Demo</h1>
         </a>
       </div>
+      <div
+        style={{ display: "flex", justifyContent: "flex-end", padding: "16px" }}
+      >
+        <ButtonTabs<ViewMode>
+          value={viewMode}
+          items={viewModes}
+          onChange={(val) => setViewMode(val)}
+        ></ButtonTabs>
+      </div>
       {tasks.length ? (
         <Gantt
           tasks={tasks}
+          viewMode={viewMode}
           todayColor="#FBEBE4"
           TooltipContent={CustomTooltip}
           TaskListHeader={TaskListHeader}
           TaskListTable={TaskListTable}
+          columnWidth={columnWidth}
+          customCalendarColumn={customCalendarColumn}
           locale="zh"
           listCellWidth="155px"
           onExpanderClick={handleExpanderClick}
+          onDateChange={handleDateChange}
+          onProgressChange={handleProgressChange}
         />
       ) : (
         ""
--- src/components/CustomTable.tsx
+++ src/components/CustomTable.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import { TaskItem } from "../apis/tasks";
 import TaskListHeader, { Header } from "./TaskListHeader";
 import TaskListTable, { Table } from "./TaskListTable";
+import ICON_EXPAND from "../assets/icon-arrow.svg";
 
 export interface CustomTableColumns<T extends keyof TaskItem> {
   title: string;
   prop: T;
+  onExpanderClick?: (task: TaskItem) => void;
   render?: (
     val: TaskItem[T],
     row: TaskItem,
@@ -16,10 +18,11 @@ function renderColumn<T extends keyof TaskItem>(
   row: TaskItem,
   column: CustomTableColumns<T>
 ): React.ReactNode {
+  const value = row[column.prop];
   if (column.render instanceof Function) {
-    return column.render(row[column.prop], row, column);
+    return column.render(value, row, column);
   }
-  return row[column.prop]?.toString();
+  return value?.toString();
 }
 export default function useCustomTable<T extends keyof TaskItem>(
   columns: CustomTableColumns<T>[]
@@ -39,6 +42,23 @@ export default function useCustomTable<T extends keyof TaskItem>(
         {(row) =>
           columns.map((column) => (
             <div className="column" key={`${row.id}-${column.prop}`}>
+              {row.children?.length &&
+              column.onExpanderClick instanceof Function ? (
+                <img
+                  src={ICON_EXPAND}
+                  onClick={() => {
+                    row.hideChildren = !row.hideChildren;
+                    console.log("点击展开按钮", row);
+                    column.onExpanderClick && column.onExpanderClick(row);
+                  }}
+                  className={`icon-expand ${
+                    row.hideChildren ? "" : "expanded"
+                  }`}
+                />
+              ) : (
+                ""
+              )}
+
               {renderColumn(row, column)}
             </div>
           ))
